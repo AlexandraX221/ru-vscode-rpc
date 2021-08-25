75d3d7157783356058bee4b607b91757d28aa118
AlexandraX <80415782+AlexandraX221@users.noreply.github.com>
2021-08-26 01:37:03 +0500

    Release

---
+++ README.md
@@ -0,0 +1,42 @@
+# VSCode Rich Presence (vscode-rpc)
+
+Расширение для кода Visual Studio, которое показывает вашу активность в текущем рабочем пространстве в Discord
+
+## Особенности
+
+* Показывает вашу текущую активность в Discord
+* Таймер активности
+
+![Скриншот 1](https://i.imgur.com/DO3fEqW.png)
+
+## Активация/Деактивация статуса
+
+Для Активации расширения вызовите командную строку(Ctrl + P на Windows, Cmd + P на Mac)
+Далее в консоли пропишите `extension.activateRich` и нажмите Enter
+Наслаждайтесь Статусом в Discord!
+
+### Важное дополнение!
+При перезапуске VSCode вам нужно будет провести процедуру повторно!
+
+Что бы Деактивировать статус вызовите командную строку(Ctrl + P на Windows, Cmd + P на Mac)
+Далее в консоли пропишите `extension.disableRich` и нажмите Enter
+
+
+
+## Как собрать расширение
+
+* [Yo VSCode Generator](https://www.npmjs.com/package/yo) - Сгенерировать шаблон расширения VS Code
+* [discord-rpc](https://www.npmjs.com/package/discord-rpc) - Discord Rich Presence API
+
+## Автор
+
+* **Matheus Pedroni** [Pedr0ni](https://twitter.com/pedr0ni_) - Разработчик расширения
+* **AlexandraX** [M&M Community Discord Server](https://dsc.gg/mmcomm) - Переводчик на русский(не официально)
+
+## Лицезия
+
+Этот проект лицензирован по лицензии MIT - см. [LICENSE.md](LICENSE.md) файл для получения подробной информации
+
+## Авотрские права
+
+Внимание! Все права на расширение пренадлежат [Pedr0ni](https://twitter.com/pedr0ni_)! В случае, если автор расширения попросит удалить его из маркетплейса, то расширение немедленно будет удалено с маркетплейса!
---
+++ src/extension.ts
@@ -0,0 +1,77 @@
+'use strict';
+import * as vscode from 'vscode';
+
+const DiscordRPC = require("discord-rpc");
+const ClientId = '466039114878418944';
+DiscordRPC.register(ClientId);
+const rpc = new DiscordRPC.Client({ transport: 'ipc' });
+
+let langs = ['typescript', 'javascript', 'php', 'css', 'html', 'json', 'markdown', 'csharp', 'aspnetcorerazor'];
+let timestamp: number;
+
+export function activate(context: vscode.ExtensionContext) {
+    console.log('Расширение Включено!');
+    timestamp = new Date().getTime();
+    let cmdActivate = vscode.commands.registerCommand('extension.activateRich', () => {
+        
+        vscode.window.showInformationMessage('Статус Активен!');
+        console.log("Инициализация RPC...");
+        rpc.login(ClientId).catch(console.error);
+        rpc.on('ready', () => {
+            console.log("RPC Запущено!");
+            updateActivity();
+        });
+
+        vscode.window.onDidChangeActiveTextEditor(editor => {
+            updateActivity();
+        });
+    });
+
+    let cmdDisable = vscode.commands.registerCommand('extension.disableRich', async () => {
+        if (!rpc) return; 
+        await rpc.destroy();
+        vscode.window.showInformationMessage('Статус Выключен!');
+    });
+
+    context.subscriptions.push(cmdActivate, cmdDisable);
+}
+
+export async function deactivate() {
+    if (!rpc) return;
+    await rpc.destroy();
+}
+
+function updateActivity() {
+    let editor = vscode.window.activeTextEditor;
+    if (editor == undefined) {
+        rpc.setActivity({
+            details: vscode.workspace.name,
+            state: "Файл не открыт",
+            largeImageKey: "undefined",
+            largeImageText: "Не Найдено",
+            smallImageKey: "logo",
+            smallImageText: "Visual Studio Code",
+            startTimestamp: timestamp,
+            instance: false
+        });
+        return;
+    }
+    let fileName = editor.document.fileName;
+    let lang: string;
+    if (langs.indexOf(editor.document.languageId) == -1) {
+        lang = "undefined";
+    } else {
+        lang = editor.document.languageId;
+    }
+    rpc.setActivity({
+        details: vscode.workspace.name,
+        state: "Редактирует " + fileName.substring(fileName.lastIndexOf("\\") + 1, fileName.length),
+        largeImageKey: lang,
+        largeImageText: lang,
+        smallImageKey: 'logo',
+        smallImageText: 'Visual Studio Code',
+        startTimestamp: timestamp,
+        instance: false,
+    });
+}
+
